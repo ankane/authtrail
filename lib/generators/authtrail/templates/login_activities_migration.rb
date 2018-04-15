c0806a81b659551e1404d415cee79523259d862c
Andrew <acekane1@gmail.com>
2018-04-14 21:28:25 -0700

    Fixed index error for MySQL - closes #7

--- lib/generators/authtrail/templates/login_activities_migration.rb
+++ lib/generators/authtrail/templates/login_activities_migration.rb
@@ -3,12 +3,12 @@ class <%= migration_class_name %> < ActiveRecord::Migration<%= migration_version
     create_table :login_activities do |t|
       t.text :scope
       t.text :strategy
-      t.text :identity
+      t.string :identity
       t.boolean :success
       t.text :failure_reason
       t.references :user, polymorphic: true
       t.text :context
-      t.text :ip
+      t.string :ip
       t.text :user_agent
       t.text :referrer
       t.text :city
@@ -19,6 +19,5 @@ class <%= migration_class_name %> < ActiveRecord::Migration<%= migration_version
 
     add_index :login_activities, :identity
     add_index :login_activities, :ip
-    add_index :login_activities, :user_id
   end
 end
