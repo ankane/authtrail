cc3f71ce474e1992f9aacf19760557df7ed5cc08
Dan Jensen <djensen@addfour.co>
2019-04-04 19:07:04 -0500

    Store latitude/longitude coordinates
    Geocoder generally returns coordinates for IP address requests. This
    modifies the geocoding job to store those values. Also updates the
    migration template and adds upgrade instructions to the documentation.

--- app/jobs/auth_trail/geocode_job.rb
+++ app/jobs/auth_trail/geocode_job.rb
@@ -10,12 +10,31 @@ module AuthTrail
         end
 
       if result
-        login_activity.update!(
+        with_coordinates = model_supports_coordinates?(login_activity)
+        attributes = attributes_for_result(result, with_coordinates)
+        login_activity.update!(attributes)
+      end
+    end
+
+    private
+
+      def attributes_for_result(result, with_coordinates)
+        attributes = {
           city: result.try(:city).presence,
           region: result.try(:state).presence,
           country: result.try(:country).presence
-        )
+        }
+        if with_coordinates
+          attributes[:latitude] = result.try(:latitude).presence
+          attributes[:longitude] = result.try(:longitude).presence
+        end
+        attributes
       end
-    end
+
+      def model_supports_coordinates?(login_activity)
+        column_names = login_activity.class.column_names
+        column_names.include?('latitude') && column_names.include?('longitude')
+      end
+
   end
 end
