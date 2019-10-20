2e73948d93441158458c95c2a974cd88a7f0e0d2
Andrew Kane <andrew@chartkick.com>
2019-10-20 13:44:34 -0700

    Fixed tests for Sprockets 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,7 @@ end
 Warden::Strategies.add(:password, PasswordStrategy)
 
 Combustion.path = "test/internal"
-Combustion.initialize! :all do
+Combustion.initialize! :active_record, :action_controller do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
