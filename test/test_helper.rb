3948fd8acbc86fde0231957f89d0fd93c410d09c
Andrew Kane <andrew@ankane.org>
2023-12-24 14:45:50 -0500

    Added config.load_defaults to test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,9 +13,7 @@ end
 
 Combustion.path = "test/internal"
 Combustion.initialize! :active_record, :action_controller, :active_job do
-  if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
-    config.active_record.sqlite3.represent_boolean_as_integer = true
-  end
+  config.load_defaults Rails::VERSION::STRING.to_f
 
   config.active_job.queue_adapter = :test
 
